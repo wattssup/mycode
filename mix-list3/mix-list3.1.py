887fbb7404cae579568ae1b84b9e33c4786b4a3a
Greg Watt <greg.watt@verizon.com>
2019-05-13 18:48:24 -0400

    List of Lists Exercize

---
+++ mix-list3/mix-list3.1.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+list1 = ['Fox', 'Fly', 'Ant', 'Bee', 'Cod', 'Cat', 'Dog', 'Yak', 'Cow' , 'Hen', 'Koi', 'Hog', 'Jay', 'Kit']
+print(f"I like {list1[0]}, {list1[5]}, {list1[6]}, {list1[8]}, {list1[13]}")
+
