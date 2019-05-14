8335071082592048ca9bcff76f17af25570525e6
Greg Watt <greg.watt@verizon.com>
2019-05-14 15:33:11 -0400

    Testing If Cond exercize

---
+++ if-test/condition02.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+hostname = input("What value should we set for hostname?")
+if hostname == "MTG":
+    print("The hostname was foubnd to be mtg")
+
---
+++ if-test/condition03.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+hostname = input("What value should we set for hostname?")
+## Notice how the next line has chanced
+## here we use the str.lower() method to return a lowercase string
+
+
+if hostname.lower() == "mtg":
+    print("The hostname was found to be mtg")
+    print("hostname matches the expected config")
+
+## Always print out to the user
+print("Exiting the script")
+
+
+
